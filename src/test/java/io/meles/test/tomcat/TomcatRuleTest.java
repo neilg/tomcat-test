fd4a2fc384e99311cb41f601dad7badee6174d3e
Neil Green <neil@melessoftware.com>
2014-02-12 20:09:27 +0000

    enhanced test coverage

--- src/test/java/io/meles/test/tomcat/TomcatRuleTest.java
+++ src/test/java/io/meles/test/tomcat/TomcatRuleTest.java
@@ -4,9 +4,6 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import org.junit.Test;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -20,11 +17,25 @@ public class TomcatRuleTest {
         final Statement base = new Statement() {
             @Override
             public void evaluate() throws Throwable {
-
                 final int localPort = ruleUnderTest.getLocalPort();
                 assertThat(localPort, is(not(0)));
-                final HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + localPort).openConnection();
-                connection.getResponseCode();
+            }
+        };
+
+        final Statement statement = ruleUnderTest.apply(base, Description.EMPTY);
+        statement.evaluate();
+    }
+
+    @Test
+    public void bindsToSpecifiedPort() throws Throwable {
+        final int portToBind = 7654;
+        final TomcatRule ruleUnderTest = new TomcatRule(portToBind);
+
+        final Statement base = new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                final int localPort = ruleUnderTest.getLocalPort();
+                assertThat(localPort, is(portToBind));
             }
         };
 
